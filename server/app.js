ce38e65dd52d30d7ab733ad37db54f7d8f5336f4
ogwurujohnson <ogwurujohnson@gmail.com>
2019-01-28 13:28:08 +0100

    [chore #163506747] Setup eslint and babel

--- server/app.js
+++ server/app.js
@@ -1,33 +1,23 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import cors from 'cors';
 import logger from 'morgan';
 import dotenv from 'dotenv';
 
-
-//import dotenv config
+// import dotenv config
 dotenv.config();
 
 const app = express();
 
-app.use(express.json());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// set router instance
-app.use(cors());
-app
+app.get('/', (req, res) => res.status(200).send({ message: 'Yay, server setup complete' }));
 
-app.length('/', (req, res) => {
-  return res.status(200).send({'message': 'Yay, server setup complete'});
-});
+const port = process.env.PORT || 7700;
 
-const port = process.env.PORT || 3000;
-
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`Application running on port ${port}`);
 });
 
 export default app;
-exports.server = server;
