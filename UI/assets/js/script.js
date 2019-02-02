7301cd7ec30496f1e04ab048a066780c8f44607f
ogwurujohnson <ogwurujohnson@gmail.com>
2019-02-02 11:01:01 +0100

    Merge changes (LFA feedbacks) back to develop

--- UI/assets/js/script.js
+++ UI/assets/js/script.js
@@ -7,9 +7,14 @@ navBarToggle.addEventListener('click', () => {
 });
 
 
-function closeNotification() {
+function closeFailedNotification() {
+  let closeBtn = document.querySelector('.closebtn');
+  let feedback = document.querySelector('.feedback2');
+  feedback.style.display = 'none';
+}
+
+function closeSuccessNotification() {
   let closeBtn = document.querySelector('.closebtn');
   let feedback = document.querySelector('.feedback');
   feedback.style.display = 'none';
 }
-
