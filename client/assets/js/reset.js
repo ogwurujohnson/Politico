d287caed781223bd61a18cbce77d78276eaf38a5
ogwurujohnson <ogwurujohnson@gmail.com>
2019-02-23 00:10:24 +0100

    split reset password from change password

--- client/assets/js/reset.js
+++ client/assets/js/reset.js
@@ -2,9 +2,6 @@
 const baseUrl = 'https://better-politico.herokuapp.com/api/v1';
 
 const email = document.querySelector('#email');
-const password = document.querySelector('#new-password');
-const confirmPassword = document.querySelector('#confirm-password');
-const submitBtn = document.querySelector('#submitBtn');
 const resetBtn = document.querySelector('#resetBtn');
 
 resetBtn.addEventListener('click', (e) => {
@@ -32,46 +29,6 @@ resetBtn.addEventListener('click', (e) => {
         console.log('email not found');
         resetBtn.value = 'Send reset email';
         resetBtn.disabled = false;
-      }
-    })
-    .catch(error => console.error(error));
-});
-
-const { search } = window.location;
-const tokenPart = search.split('=');
-
-submitBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-
-  const userData = {
-    email: localStorage.getItem('userEmail'),
-    password: password.value,
-    resetTokenHash: tokenPart[1],
-  };
-
-  submitBtn.value = 'Resetting Password >>>>>';
-  submitBtn.disabled = true;
-
-  fetch(`${baseUrl}/auth/validate`, {
-    method: 'POST',
-    body: JSON.stringify(userData),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then(res => res.json())
-    .then((response) => {
-      if (response.status === 200) {
-        const loginUrl = '../../pages/auth/sign-in.html';
-        window.location.href = loginUrl;
-      } else if (response.status === 404) {
-        console.log('email not found');
-        submitBtn.value = 'Change Password';
-        submitBtn.disabled = false;
-      } else if (response.status === 400) {
-        console.log('Check that the token is corect');
-        submitBtn.value = 'Change Password';
-        submitBtn.disabled = false;
       }
     })
     .catch(error => console.error(error));
