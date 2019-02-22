645e991a231a5e50919b6fca04fcfde97575803d
ogwurujohnson <ogwurujohnson@gmail.com>
2019-02-22 18:52:38 +0100

    feat(Authorization): Implement User signup and login
    - Project setup
    - Image upload with cloudinary
    - Consume signup API
    - Consume login Api
    - Redirect user to profile or admin dashboard page if successful

---
+++ client/assets/js/script.js
@@ -0,0 +1,26 @@
+let mainNav = document.querySelector('#menu');
+
+let navBarToggle = document.querySelector('#toggle');
+
+navBarToggle.addEventListener('click', () => {
+  mainNav.classList.toggle('active');
+});
+
+
+function closeFailedNotification() {
+  let closeBtn = document.querySelector('.closebtn');
+  let feedback = document.querySelector('.feedback2');
+  feedback.style.display = 'none';
+}
+
+function closeSuccessNotification() {
+  let closeBtn = document.querySelector('.closebtn');
+  let feedback = document.querySelector('.feedback2');
+  feedback.style.display = 'none';
+}
+
+function closeSuccessNotification() {
+  let closeBtn = document.querySelector('.closebtn');
+  let feedback = document.querySelector('.feedback');
+  feedback.style.display = 'none';
+}
